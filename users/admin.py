1e3ebcb923de3c30621929603e9071900b096b25
hmg612 <hmgcjn@gmail.com>
2020-02-26 16:29:36 +0900

    admin and user app 2/26

---
+++ users/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+# Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+
+# admin.site.register(models.User, CustomUserAdmin) <- 위의 @표시 대신 이렇게 사용가능
