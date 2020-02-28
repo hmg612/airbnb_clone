9fe842f0d6a83d8608eced8c772671d7d2926e3c
hmg612 <hmgcjn@gmail.com>
2020-02-29 01:49:32 +0900

    adding random seed in database 2/28

--- rooms/models.py
+++ rooms/models.py
@@ -3,8 +3,6 @@ from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
 
-# Create your models here.
-
 
 class AbstractItem(core_models.TimeStampedModel):
 
@@ -28,7 +26,7 @@ class RoomType(AbstractItem):
         ordering = ["created"]
 
 
-class Amentiny(AbstractItem):
+class Amenity(AbstractItem):
 
     """ Amenity Model Definition """
 
@@ -51,16 +49,18 @@ class HouseRule(AbstractItem):
     class Meta:
         verbose_name = "House Rule"
 
+
 class Photo(core_models.TimeStampedModel):
 
     """ Photo Model Definition """
 
     caption = models.CharField(max_length=80)
-    file = models.ImageField()
-    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    file = models.ImageField(upload_to="room_photos")
+    room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.caption
+
 
 class Room(core_models.TimeStampedModel):
 
@@ -79,11 +79,28 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField("Amentiny", blank=True)
-    facilities = models.ManyToManyField("Facility", blank=True)
-    house_rules = models.ManyToManyField("HouseRule", blank=True)
+    host = models.ForeignKey(
+        "users.User", related_name="rooms", on_delete=models.CASCADE
+    )
+    room_type = models.ForeignKey(
+        "RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.city = str.capitalize(self.city)
+        super().save(*args, **kwargs)
+
+    def total_rating(self):
+        all_reviews = self.reviews.all()
+        all_ratings = 0
+        if len(all_reviews) > 0:
+            for review in all_reviews:
+                all_ratings += review.rating_average()
+            return all_ratings / len(all_reviews)
+        return 0
