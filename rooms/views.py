ee29425c7de83b02f8d3fb642e93e11d5bd82c4b
hmg612 <hmgcjn@gmail.com>
2020-03-03 01:08:44 +0900

    Search View 3/2

--- rooms/views.py
+++ rooms/views.py
@@ -3,6 +3,8 @@
 # from django.urls import reverse
 # from django.shortcuts import render # redirect
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 
@@ -19,11 +21,55 @@ class Homeview(ListView):
 
 class RoomDetail(DetailView):
 
-    """ RoomDetail Definition """ 
+    """ RoomDetail Definition """
 
     model = models.Room
 
 
+def search(request):
+    # print(request.GET)
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    selected_amenities = request.GET.getlist("amenities")
+    selected_facilities = request.GET.getlist("facilities")
+    print(selected_amenities, selected_facilities)
+
+    form = {
+        "city": city,
+        "selected_room_type": room_type,
+        "selected_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "selected_amenities": selected_amenities,
+        "selected_facilities": selected_facilities,
+        "instant": instant,
+        "super_host": super_host,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    return render(request, "rooms/search.html", {**form, **choices})
 
 
 # HomeView - Pagination manual coding 1
