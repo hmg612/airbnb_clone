95f8af2fd84c4b6f13efcc2d195485fedef3041e
hmg612 <hmgcjn@gmail.com>
2020-03-13 02:09:19 +0900

    main page & login page design 3/12

--- rooms/views.py
+++ rooms/views.py
@@ -14,7 +14,7 @@ class Homeview(ListView):
     """ Homeview Definition with class based view """
 
     model = models.Room
-    paginate_by = 10
+    paginate_by = 12
     paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
